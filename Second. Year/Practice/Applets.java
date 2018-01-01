adfeb6ad19356a64044b630f8f9798bd9fabb122
Parshuram reddy <pandu@Parshurams-MacBook-Pro.local>
2018-01-01 22:58:22 +0530

    Second Year Pro Started: ;

---
+++ Second. Year/Practice/Applets.java
@@ -0,0 +1,12 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+public class Applets extends Applet
+{
+
+    public void paint(Graphics g)
+
+    {
+        g.drawString("Welcome To First",150,150);
+    }
+
+}
